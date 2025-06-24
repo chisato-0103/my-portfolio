9a8c372577ec538c0770c2278468d7a1ee2fade7
chisato <x24014xx@aitech.ac.jp>
2025-06-24 21:45:48 +0900

    東京ゲームショウプロジェクトの実際のGitHubリンクを追加
    - Projects.tsx: TGS2025リポジトリへのリンクを追加
    - 実際のプロジェクトリポジトリにアクセスできるように修正

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
             </div>
             <div className="project-links">
               <a
-                href="https://github.com/chisato-0103"
+                href="https://github.com/chisato-0103/TGS2025.git"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="project-link"
