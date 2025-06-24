efc9f7ab0adecf7ca60f42882dcefbef7f7c50fb
chisato <x24014xx@aitech.ac.jp>
2025-06-24 20:15:49 +0900

    style: コードフォーマッターによる自動整形

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // GitHub Pagesのリポジトリ名に合わせる
+  base: "/my-portfolio/", // GitHub Pagesのリポジトリ名に合わせる
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
   },
-})
+});
